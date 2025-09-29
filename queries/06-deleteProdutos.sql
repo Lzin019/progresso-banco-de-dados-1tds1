9e4b5f3f8be40ea23dafe76546a78f374a774af3
Luiz felipe <seuemail@example.com>
2025-09-29 14:09:08 -0300

    Commit final

--- queries/06-deleteProdutos.sql
+++ queries/06-deleteProdutos.sql
@@ -11,4 +11,10 @@ SELECT * FROM produtos;
 DELETE FROM produtos
 WHERE marca = 'Pantene';
 
+DELETE FROM produtos 
+WHERE estoque > 250 AND estoque < 400;
+
+DELETE FROM produtos 
+WHERE estoque BETWEEN 250 AND 400;
+
 SELECT * FROM produtos;
